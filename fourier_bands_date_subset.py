2160766a8372b04e222b01867f03997c78e58e71
Rob Webster <robwebster@mailbox.org>
2021-01-08 11:48:04 +0000

    debug and add option to collate all files

--- fourier_bands_date_subset.py
+++ fourier_bands_date_subset.py
@@ -11,7 +11,9 @@ import subprocess
 @click.option('--years', default='2020', type=click.STRING, help="years to include - comma separated eg 2018,2019,2020")
 @click.option('--months',default='1,2,3,4,5,6,7,8,9,10,11,12' , type=click.STRING, help="months to include, comma separated eg 3,4,5,6,7,8,")
 @click.option('--indices', type=click.Path(exists=True), default='/home/nx06/nx06/shared/RF/S2AWS/indices_to_process.csv', help='CSV of indices to process')
-def main(project, tiles, years, months, indices):
+@click.option('--suffix', default='', type=click.STRING, help='A suffix to add to to the end of the bands and indices files so as to keep them separate.  Default is no suffix')
+@click.option('--collate_all_files', default=True, is_flag=True, help='If set, create a single text file with all files to process')
+def main(project, tiles, years, months, indices, suffix, collate_all_files):
 
     with open(indices, newline='\n') as f:
         reader = csv.reader(f)
@@ -46,25 +48,30 @@ def main(project, tiles, years, months, indices):
 
             for band in bands:
 
-                band_file = f"{os.environ['MAINDIR']}/S2AWS/{aoi}/Fourier_Files_{band}_subset.txt"
+                band_file = f"{os.environ['MAINDIR']}/S2AWS/{aoi}/Fourier_Files_{band}{suffix}.txt"
 
                 subprocess.run(f"touch {band_file}", shell=True)
 
                 for year in years:
                     for month in months:
-                        subprocess.run(f"ls {os.environ['MAINDIR']}/S2AWS/{aoi}/SAFE/*L2A_{year}{month}*/S2Fmask/R{resolution}m/*{band}*.tif >> {os.environ['MAINDIR']}/S2AWS/{aoi}/Fourier_Files_{band}_subset.txt", shell=True)
+                        subprocess.run(f"ls {os.environ['MAINDIR']}/S2AWS/{aoi}/SAFE/*L2A_{year}{month}*/S2Fmask/R{resolution}m/*{band}*.tif >> {os.environ['MAINDIR']}/S2AWS/{aoi}/Fourier_Files_{band}{suffix}.txt", shell=True)
 
 
 
             for index in indices_list:
 
-                index_file = f"{os.environ['MAINDIR']}/S2AWS/{aoi}/Fourier_Files_{index}_subset.txt"
+                index_file = f"{os.environ['MAINDIR']}/S2AWS/{aoi}/Fourier_Files_{index}{suffix}.txt"
 
                 subprocess.run(f"touch {index_file}", shell=True)
 
                 for year in years:
                     for month in months:
-                        subprocess.run(f"ls {os.environ['MAINDIR']}/S2AWS/{aoi}/SAFE/*L2A_{year}{month}*/indices/*_{index}.tif >> $MAINDIR/S2AWS/{aoi}/Fourier_Files_{index}_subset.txt", shell=True)
+                        subprocess.run(f"ls {os.environ['MAINDIR']}/S2AWS/{aoi}/SAFE/*L2A_{year}{month}*/indices/*_{index}.tif >> $MAINDIR/S2AWS/{aoi}/Fourier_Files_{index}{suffix}.txt", shell=True)
+
+        
+        if collate_all_files:
+            fourier_files_path=f"/home/nx06/nx06/shared/RF/Projects/{project}/file_lists/{project}_Fourier_Files{suffix}.txt"
+            subprocess.run(f"ls -1 {os.environ['MAINDIR']}/S2AWS/{project}_?????/Fourier_Files*{suffix}.txt > {fourier_files_path}", shell=True)
 
 if __name__ == "__main__":
     main()
